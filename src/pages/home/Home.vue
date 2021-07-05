9f5d929d191f3aee1ecb9e5c5016a39e377c4e9d
fwnok001 <fwnok001@163.com>
2021-07-05 12:03:38 +0800

    再次修改

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "这里要开始测试项目了",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
